6218c954ab597b812b1f318282b8a35cb9de0dea
Faisal <faisalsagheerqureshi@gmail.com>
2025-07-27 14:21:14 +0500

    sphere

---
+++ src/components/design/Sphere.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Sphere() {
+    return (
+        <div>
+            <div class="sphere">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Sphere
